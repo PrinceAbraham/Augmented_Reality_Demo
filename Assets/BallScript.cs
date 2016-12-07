a25f170e55c9a32cf79cd673082fdad73d1bff11
Prince <prince.vabraham@gmail.com>
2016-12-07 14:32:22 -0500

    Assets/Import Packages Characters Ice
    Maze Ready. Need to work on the time and winning and sometimes breaks.
    Maybe Save state with DewfaultTrackableEventHandeler

---
+++ Assets/BallScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+	public GameObject Floor;
+	public GameObject SpawnPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+			//gameObject.transform.position = new Vector3 (gameObject.transform.position.x, 0.03f,gameObject.transform.position.z);
+
+		if (gameObject.transform.position.y < Floor.transform.position.y - 10f) {
+			gameObject.transform.position = SpawnPoint.transform.position;
+		}
+	}
+}
